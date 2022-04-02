212c9d6713e6806c00e447d9eb776cd6f0cbeee5
angsarkuanysh <99543461+angsarkuanysh@users.noreply.github.com>
2022-04-02 08:57:27 +0600

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		$a=45;
+		$b=25;
+		$c=$a+$b;
+		echo $a,' + ',$b,' = ', $c;
+		$d=7;
+		$result=$c+$d;
+		echo '  ',$c,' + ', $d, ' = ', $result;
+	?>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		$text1='Салем';
+		$text2='Алем!';
+		echo $text1, ' ',$text2;
+	?>
+</body>
+</html>
